44c3bca799c7e691d0eabb7c5750c76298456764
vgthorat24 <162952465+vgthorat24@users.noreply.github.com>
2024-03-12 23:13:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Conuter from './components/Conuter';
 
 function App() {
+  const addition=()=>{
+    let a=10;
+    let b=20;
+    let total=a+b;
+  }
   return (
     <div className="App">
      <Conuter/>
