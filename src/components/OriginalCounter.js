41ed3024e05274997831269c21d75128e5c6a180
vaibhavthorat115@gmail.com <vaibhavthorat115@gmail.com>
2024-03-12 23:44:26 +0530

    OriginalCounter JS file Added

---
+++ src/components/OriginalCounter.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react';
+
+const OriginalCounter=()=>{
+  const [counter,setCounter]=useState(0);
+  const handleIncreament=()=>{
+    setCounter(counter+1);
+  }
+  return (
+    <>
+      <h1>This Is Normal Counter </h1>
+      <h2>Count is: {counter}</h2>
+  <button onClick={handleIncreament}>Increament</button>
+    </>
+  )
+}
